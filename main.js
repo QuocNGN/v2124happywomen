3e271f0c38f68f19476731398cf7589d98df6160
QuocNQN <quoc.nng2002@gmail.com>
2024-10-20 18:00:50 +0700

    Update Music

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
+window.addEventListener('load', () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove('not-loaded');
+    clearTimeout(c);
+  }, 1000);
+
+  const audioElement = document.getElementById('background-music');
+
+  // Giảm âm lượng xuống còn 30% (tức là nhỏ lại 70%)
+  audioElement.volume = 0.3;
+
+  audioElement.play();
+
+  audioElement.onended = function () {
+    audioElement.currentTime = 0;
+    audioElement.play();
   };
+});
